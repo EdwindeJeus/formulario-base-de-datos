44f69fe00cf8e2c1ec41658d9aa52430f96f1def
EdwindeJeus <126724657+EdwindeJeus@users.noreply.github.com>
2023-04-20 21:56:06 -0600

    Add files via upload

---
+++ init_ems_p.php
@@ -0,0 +1,7 @@
+<?php
+require 'include/consult_serv.php';
+
+$obtener_datos = consultarDatos('ems_plant');
+
+echo json_encode($obtener_datos);
+
---
+++ init_of_ed.php
@@ -0,0 +1,7 @@
+
+<?php
+require 'include/consult_serv.php';
+
+$obtener_datos = consultarDatos('carreras');
+
+echo json_encode($obtener_datos);
